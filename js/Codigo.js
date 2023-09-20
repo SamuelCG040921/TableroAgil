e07af106750af42a1dc2856134b86aae106a7954
Juan Jose Tovar <juanjosetovar330@gmail.com>
2023-09-20 16:45:15 -0500

    Agregada funcionalidad editar y eliminar tarea

--- js/Codigo.js
+++ js/Codigo.js
@@ -25,6 +25,7 @@ function crearTarea(event) {
             btnEditar.classList.add('btn-crear')
         } else {
             tarea.nombre = document.getElementById("tarea-nombre").value
+            registrarTarea()
         }
     }
 
@@ -69,6 +70,36 @@ function registrarTarea() {
     pNombre.setAttribute('id', 'nombre')
     pNombre.textContent = tarea.nombre
 
+    const inputEditar = document.createElement('input')
+    inputEditar.classList.add('btn-crear')
+    inputEditar.setAttribute('type', 'submit')
+    inputEditar.value = 'Editar'
+    inputEditar.onclick = function() {
+        isEditando = true
+        tarea.id = divTarea.getAttribute('id')
+        tarea.nombre = pNombre.textContent
+        editarTarea()
+    }
+
+    const inputBorrar = document.createElement('input')
+    inputBorrar.classList.add('btn-borrar')
+    inputBorrar.setAttribute('type', 'submit')
+    inputBorrar.value = 'Borrar'
+    inputBorrar.onclick = function() {
+        divTarea.remove()
+    }
+
     pendientes.appendChild(divTarea)
+    divTarea.appendChild(inputEditar)
+    divTarea.appendChild(inputBorrar)
     divTarea.appendChild(pNombre)
+}
+
+function editarTarea() {
+    const btnEditar = document.getElementById("btn-crear-editar")
+    btnEditar.value = "Editar Tarea"
+    btnEditar.classList.remove('btn-crear')
+    btnEditar.classList.add('btn-editar')
+
+    document.getElementById("tarea-nombre").value = tarea.nombre
 }
